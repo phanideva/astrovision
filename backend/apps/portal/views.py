efd4a7dae6fbecb14544874702a9de239195e8a7
phanideva <phanisaisri@gmail.com>
2026-05-16 10:25:01 -0500

    removed unused imports

--- backend/apps/portal/views.py
+++ backend/apps/portal/views.py
@@ -1,6 +1,5 @@
-from django.db.models import Q
 from django.utils import timezone
-from rest_framework import mixins, status, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
